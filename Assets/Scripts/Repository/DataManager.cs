77044ca4c4154f518ffb2e6fb80b2a210328a85d
NivSaparov <nivsaparov1@gmail.com>
2021-10-30 20:58:57 +0300

    fix: variables name convention

--- Assets/Scripts/Repository/DataManager.cs
+++ Assets/Scripts/Repository/DataManager.cs
@@ -6,43 +6,43 @@ using System.IO;
 
 public class DataManager
 {
-    private readonly NetworkImpl network;
-    private List<Result> videoClipsDataset;
+    private readonly NetworkImpl Network;
+    private List<Result> VideoClipsDataset;
     private const string VIDEO_CLIPS_URL = "https://run.mocky.io/v3/81fab340-9550-4ab4-8859-836b01ee48ff";
-    private Action startPlayingCallback;
-    private static readonly string videoDataFile = "dataObject.fun";
+    private Action StartPlayingCallback;
+    private static readonly string VideoDataFilename = "dataObject.fun";
     public DataManager(NetworkImpl network, Action startPlayingCallback)
     {
-        this.network = network;
-        this.startPlayingCallback = startPlayingCallback;
+        Network = network;
+        StartPlayingCallback = startPlayingCallback;
     }
 
-    public IEnumerator FetchVideos() => network.Get<Root>(VIDEO_CLIPS_URL, OnDataReceived);
+    public IEnumerator FetchVideos() => Network.Get<Root>(VIDEO_CLIPS_URL, OnDataReceived);
 
-    public Result GetCurrent(int index) => videoClipsDataset[index];
+    public Result GetCurrent(int index) => VideoClipsDataset[index];
 
-    public int DatasetCount() => videoClipsDataset.Count;
+    public int DatasetCount() => VideoClipsDataset.Count;
     private void OnDataReceived(Root videos)
     {
         /**
          * Once we receive data, we get the local data in order to compare and start downloading missing videos.
          * We download while streaming for a better user experience.       
          */
-        var localDataVideos = LocalDataImpl.GetList<Result>(videoDataFile);
+        var localDataVideos = LocalDataImpl.GetList<Result>(VideoDataFilename);
         var diffDataVideos = DiffVidedoData(localDataVideos,videos.results);
-        this.videoClipsDataset = MergeVideoList(localDataVideos, diffDataVideos);
-        startPlayingCallback();
+        this.VideoClipsDataset = MergeVideoList(localDataVideos, diffDataVideos);
+        StartPlayingCallback();
         DownloadNewFiles();
     }
 
     public IEnumerator DownloadNewFiles()
     {
-        foreach(var video in videoClipsDataset)
+        foreach(var video in VideoClipsDataset)
         {
             if (!video.isCached)
             {
                 var filename = video.id + Path.GetExtension(video.video_url);
-                yield return network.Download(video.video_url, filename, SaveFile);
+                yield return Network.Download(video.video_url, filename, SaveFile);
             }
         }
         yield return null;
@@ -55,7 +55,7 @@ public class DataManager
         string newPath = LocalDataImpl.SaveFile(data, filename);
         string fileId = Path.GetFileNameWithoutExtension(filename);
         // Update dataset
-        Result item = videoClipsDataset.Find(item => item.id == fileId);
+        Result item = VideoClipsDataset.Find(item => item.id == fileId);
         if(item != null)
         {
             item.video_url = newPath;
@@ -63,7 +63,7 @@ public class DataManager
         }
 
         // Update localdb
-        LocalDataImpl.SaveList(videoClipsDataset, videoDataFile);
+        LocalDataImpl.SaveList(VideoClipsDataset, VideoDataFilename);
 
 
     }
@@ -108,7 +108,7 @@ public class DataManager
         return diffData;
     }
 
-    public IEnumerator SendTracking(string trackingURL, string deafultPlayerID, string playerID) => network.Get(TrackingURLWithPlayerID(trackingURL, deafultPlayerID, playerID));
+    public IEnumerator SendTracking(string trackingURL, string deafultPlayerID, string playerID) => Network.Get(TrackingURLWithPlayerID(trackingURL, deafultPlayerID, playerID));
     private string TrackingURLWithPlayerID(string trackingURL, string deafultPlayerID, string playerID) => trackingURL.Replace(deafultPlayerID, playerID);
 
 
